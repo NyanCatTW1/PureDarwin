87f6e84f00cce3bc579580e6329e36069e909113
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2025-06-06 17:10:12 +0800

    iig: Fix clangFlags parsing and add printTranslationUnit

--- tools/iig/iig.cpp
+++ tools/iig/iig.cpp
@@ -1,4 +1,5 @@
 #include "iig.h"
+#include <vector>
 
 static void usage(const char *progname) {
     std::cerr
@@ -13,16 +14,79 @@ static void usage(const char *progname) {
     exit(1);
 }
 
+void printTranslationUnit(CXTranslationUnit source) {
+    CXCursor cursor = clang_getTranslationUnitCursor(source);
+
+    clang_visitChildren(
+        cursor,
+        [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
+            CXType cursor_type = clang_getCursorType(current_cursor);
+
+            CXString type_kind_spelling =
+                clang_getTypeKindSpelling(cursor_type.kind);
+            std::cout << "TypeKind: " << clang_getCString(type_kind_spelling);
+            clang_disposeString(type_kind_spelling);
+
+            if (cursor_type.kind ==
+                    CXType_Pointer || // If cursor_type is a pointer
+                cursor_type.kind ==
+                    CXType_LValueReference || // or an LValue Reference (&)
+                cursor_type.kind ==
+                    CXType_RValueReference) { // or an RValue Reference (&&),
+                CXType pointed_to_type = clang_getPointeeType(
+                    cursor_type); // retrieve the pointed-to type
+
+                CXString pointed_to_type_spelling = clang_getTypeSpelling(
+                    pointed_to_type); // Spell out the entire
+                std::cout << "pointing to type: "
+                          << clang_getCString(
+                                 pointed_to_type_spelling); // pointed-to type
+                clang_disposeString(pointed_to_type_spelling);
+            } else if (cursor_type.kind == CXType_Record) {
+                CXString type_spelling = clang_getTypeSpelling(cursor_type);
+                std::cout << ", namely " << clang_getCString(type_spelling);
+                clang_disposeString(type_spelling);
+            }
+            std::cout << "\n";
+            return CXChildVisit_Recurse;
+        },
+        nullptr);
+
+    clang_visitChildren(
+        cursor,
+        [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
+            CXType cursor_type = clang_getCursorType(current_cursor);
+            CXString cursor_spelling = clang_getCursorSpelling(current_cursor);
+            CXSourceRange cursor_range = clang_getCursorExtent(current_cursor);
+            std::cout << "Cursor " << clang_getCString(cursor_spelling);
+
+            CXFile file;
+            unsigned start_line, start_column, start_offset;
+            unsigned end_line, end_column, end_offset;
+
+            clang_getExpansionLocation(clang_getRangeStart(cursor_range), &file,
+                                       &start_line, &start_column,
+                                       &start_offset);
+            clang_getExpansionLocation(clang_getRangeEnd(cursor_range), &file,
+                                       &end_line, &end_column, &end_offset);
+            std::cout << " spanning lines " << start_line << " to " << end_line;
+            clang_disposeString(cursor_spelling);
+
+            std::cout << "\n";
+            return CXChildVisit_Recurse;
+        },
+        nullptr);
+}
+
 int main(int argc, const char *argv[]) {
     int exitCode = 0;
     string inputFilePath, headerOutputPath, implOutputPath;
-    string clangFlags;
+    vector<const char *> clangFlags;
     bool seenDashDash = false;
 
     for (int i = 1; i < argc; i++) {
         if (seenDashDash) {
-            clangFlags += argv[i];
-            clangFlags += " ";
+            clangFlags.push_back(argv[i]);
         } else if (strequal(argv[i], "--def")) {
             if (++i == argc) {
                 std::cerr << "iig: error: --def option requires an argument"
@@ -82,9 +146,10 @@ int main(int argc, const char *argv[]) {
     }
 
     CXIndex index = clang_createIndex(0, 0);
-    const char *clangFlagsStr = clangFlags.c_str();
+
     CXTranslationUnit source = clang_createTranslationUnitFromSourceFile(
-        index, inputFilePath.c_str(), 1, &clangFlagsStr, 0, nullptr);
+        index, inputFilePath.c_str(), clangFlags.size(), clangFlags.data(), 0,
+        nullptr);
 
     size_t num_diags = clang_getNumDiagnostics(source);
     if (num_diags > 0) {
@@ -127,6 +192,8 @@ int main(int argc, const char *argv[]) {
         if (found_error)
             exit(1);
     }
+
+    printTranslationUnit(source);
 
     std::cerr << "iig: fatal error: iig is not implemented at this time."
               << std::endl;
